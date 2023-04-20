115c9637d69821bf0ea768333e06565a4f7457f5
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-04-20 02:36:33 +0200

    organized the code and made date changes checks evrey 2 hours

--- background.js
+++ background.js
@@ -1,37 +1,98 @@
+//update date evrey time opens the browser
+chrome.windows.onCreated.addListener(updateDate)
 
-//will run when user open his browser
+//check if website is banned or no when a tab is updated
 chrome.tabs.onCreated.addListener(function (tab) {
     chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, updatedTab) {
         if (changeInfo.status == "loading" && changeInfo.url) {
             checkCurrent(changeInfo.url, tabId)
-            console.log("checking")
         }
     });
 });
 
-//will run when a new tab is created
-chrome.windows.onCreated.addListener(function () {
+//checking current website is banned and starting the time if it is
+async function checkCurrent(currentUrl, id) {
+    const data = await chrome.storage.local.get(["bannedSites"]) ?? []
+    const bannedUrls = await data.bannedSites
+    if (isBanned(currentUrl, bannedUrls)) {
+        //starting timer or closing the tab
+        chrome.storage.local.get(["timeLeft"]).then(data => {
+            if (data.timeLeft && data.timeLeft > 0) {
+                //start timer(data.timeLeft)
+                startTimer(data.timeLeft, id, bannedUrls)
+            } else {
+                try {
+                    chrome.tabs.remove(id, () => {
+                        console.log('')
+                    })
+                } catch (error) { }
+            }
+        })
+    }
+}
+
+//updating active tab whenever a new website get banned
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let key in changes) {
+        if (key === "bannedSites") {
+            try {
+                chrome.tabs.query({ active: true }).then(data => {
+                    chrome.tabs.remove(data[0].id, () => {
+                        console.log('')
+                    })
+                    chrome.tabs.create({ url: data[0].url });
+
+                })
+            } catch (error) {
+                console.log('')
+            }
+
+        }
+    }
+});
+
+//going to implement time reset check evrey 1 hour
+setInterval(updateDate, 7200000);
+//functions
+
+function startTimer(timeLeft, id, bannedUrls) {
+    const intervaleId = setInterval(() => {
+        chrome.tabs.query({ active: true, highlighted: true }).then(tabs => {
+            if (tabs[0].id == id && isBanned(tabs[0].url, bannedUrls)) {
+                timeLeft--
+                chrome.storage.local.set({ "timeLeft": timeLeft })
+            }
+        })
+        if (timeLeft <= 1) {
+            try {
+                chrome.tabs.remove(id, () => {
+                    console.log('')
+                })
+                clearInterval(intervaleId)
+            } catch (err) { clearInterval(intervaleId) }
+
+        }
+    }, 60000)
+}
+
+function updateDate() {
     const date = new Date()
     const today = date.getDay().toString() + date.getFullYear().toString()
     chrome.storage.local.get(["lastDate"])
         .then(data => {
             if (data.lastDate != today) {
-                //this reset the timer evrey day
                 chrome.storage.local.set({ "lastDate": today }).then(data => console.log("updated lastDate"))
                 chrome.storage.local.get(["timeLimit"]).then(data => {
                     const timeLimit = data.timeLimit ?? 60
                     chrome.storage.local.set({ "timeLeft": timeLimit }).then(data => console.log("reseted time left "))
                 })
             }
-
         })
-})
+}
 
 function extractDomainName(url) {
-    // Remove any protocol and www from the URL
     let domain = url.replace(/(^\w+:|^)\/\/(www\.)?/, '');
 
-    // Remove any path or query parameters
     domain = domain.split('/')[0];
 
     return domain;
@@ -48,51 +109,3 @@ function isBanned(url, bannedUrls) {
     return banned
 
 }
-//checking current website is banned and starting the time if it is
-async function checkCurrent(currentUrl, id) {
-    const data = await chrome.storage.local.get(["bannedSites"]) ?? []
-    const bannedUrls = await data.bannedSites
-    if (isBanned(currentUrl, bannedUrls)) {
-        //starting timer or closing the tab
-        chrome.storage.local.get(["timeLeft"]).then(data => {
-            if (data.timeLeft && data.timeLeft > 0) {
-                const intervaleId = setInterval(() => {
-                    chrome.tabs.query({ active: true, highlighted: true }).then(tabs => {
-                        if (tabs[0].id == id && isBanned(tabs[0].url, bannedUrls)) {
-                            data.timeLeft--
-                            chrome.storage.local.set({ "timeLeft": data.timeLeft })
-                        }
-                    })
-                    if (data.timeLeft <= 2) {
-                        try {
-                            chrome.tabs.remove(id, () => {
-                                console.log('')
-                            })
-                            clearInterval(intervaleId)
-                        } catch (err) { clearInterval(intervaleId) }
-
-                    }
-                }, 60000)
-            } else {
-                try {
-                    chrome.tabs.remove(id, () => {
-                        console.log('')
-                    })
-                } catch (error) { }
-            }
-        })
-    }
-}
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-    for (let key in changes) {
-        if (key === "bannedSites") {
-            chrome.tabs.query({ active: true }).then(data => {
-                chrome.tabs.remove(data[0].id, () => {
-                    console.log('')
-                })
-                chrome.tabs.create({ url: data[0].url });
-
-            })
-        }
-    }
-});
